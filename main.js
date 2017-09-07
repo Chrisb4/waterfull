ed347bdb8b5cbe5083974e7ddaefcd8d56491a37
Christopher Brown <nudgechris@gmail.com>
2017-09-06 21:57:35 -0500

    still flexin

--- main.js
+++ main.js
@@ -33,3 +33,32 @@ water.addEventListener('change', function(e) {
   updateOutput(waterVal);
 });
 
+const waterForm2 = document.getElementById('waterForm2');
+const button2 = document.getElementById('button2');
+const butt2 = document.getElementById('click-target2');
+const target2 = document.getElementById('target2')
+const water2 = document.querySelector('input[name=water]');
+const output2 = document.querySelector('#output');
+console.log(output2);
+let waterDrank2 = 0;
+
+
+function updateOutput(vol) {
+  output.value = vol;
+}
+
+function targetUpdate(waterDrank) {
+  const template = `<h1>${parseInt(waterDrank)} ounces</h1>`
+  target.innerHTML = template;
+}
+
+button.addEventListener('click', function(e) {
+  e.preventDefault()
+  waterDrank = parseInt(waterDrank) + parseInt(water.value);
+  targetUpdate(waterDrank);
+});
+
+water.addEventListener('change', function(e) {
+  const waterVal = water.value;
+  updateOutput(waterVal);
+});
